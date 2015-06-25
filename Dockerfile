13e17bfb93fd85ceef874dc970150732baca5485
Marc Lennox <marc.lennox@gmail.com>
2015-06-25 10:58:51 -0400

    Added support for multiple TCP and SSH tunnels

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ FROM gliderlabs/alpine:latest
 MAINTAINER Marc Lennox <marc.lennox@gmail.com>
 
 # Install socat
-RUN apk --update add socat openssh
+RUN apk --update add socat openssh sshpass
 
 # Add files
 COPY ./tunnel.sh /usr/local/bin/tunnel
--- tunnel.sh
+++ tunnel.sh
@@ -1,43 +1,31 @@
 #!/bin/sh
 set -e
 
-# Look for a TCP and SSH tunnel spec in the environment
-tcp_tunnel_spec=$(env | grep _TCP= | head | sed --regexp-extended 's/.*_PORT_([0-9]+)_TCP=(tcp:\/\/)?(.+):([0-9]+)/echo \1 \3 \4/' | sh)
-ssh_tunnel_spec=$(env | grep _SSH= | head | sed --regexp-extended 's/.*_PORT_([0-9]+)_SSH=(ssh:\/\/)?(.+):([0-9]+)/echo \1 \3 \4/' | sh)
+# Load environment variables
+SSH_SERVER_CHECK_INTERVAL=${SSH_SERVER_CHECK_INTERVAL:-30}
 
-# Return if no tunnel spec found
-if [ -z "$tcp_tunnel_spec" ] && [ -z "$ssh_tunnel_spec" ]; then
-  echo "Tunnel specification not found in environment"
-  exit 1
-fi
+# Look for insecure tunnel specifications and spawn them
+env | grep -E '^TCP_TUNNEL_PORT_([0-9]+)=' | sed -E 's/^TCP_TUNNEL_PORT_([0-9]+)=(.+):([0-9]+)/\1 \2 \3/' | while read -r local_port remote_host remote_port ; do
+  echo "Opening insecure tunnel *:$local_port -> $remote_host:$remote_port"
+  /usr/bin/socat TCP4-LISTEN:$local_port,fork,reuseaddr TCP4:$remote_host:$remote_port &
+  pids="$pids $!"
+done
 
-if ! [ -z "$tcp_tunnel_spec" ]; then
+# Look for secure tunnel specifications and spawn them
+env | grep -E '^SSH_TUNNEL_PORT_([0-9]+)=' | sed -E 's/^SSH_TUNNEL_PORT_([0-9]+)=(.+):(.+)@(.+):([0-9]+)/\1 \2 \3 \4 \5/' | while read -r local_port user credentials remote_host remote_port ; do
 
-  # Extract invididual variables
-  LOCAL_PORT=$(echo $tcp_tunnel_spec | cut -d' ' -f1)
-  REMOTE_HOST=$(echo $tcp_tunnel_spec | cut -d' ' -f2)
-  REMOTE_PORT=$(echo $tcp_tunnel_spec | cut -d' ' -f3)
-
-  # Open insecure tunnel with socat
-  echo "Opening insecure tunnel *:${LOCAL_PORT} -> ${REMOTE_HOST}:${REMOTE_PORT}"
-  exec socat TCP4-LISTEN:${LOCAL_PORT},fork,reuseaddr TCP4:${REMOTE_HOST}:${REMOTE_PORT}
-
-else
-
-  # Extract invididual variables
-  LOCAL_PORT=$(echo $ssh_tunnel_spec | cut -d' ' -f1)
-  REMOTE_HOST=$(echo $ssh_tunnel_spec | cut -d' ' -f2)
-  REMOTE_PORT=$(echo $ssh_tunnel_spec | cut -d' ' -f3)
-
-  # Open secure tunnel with SSH
-  ssh_command="usr/bin/ssh -T -N -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=false -o ServerAliveInterval=30"
-  if [ -f "${SSH_USER}" ]; then
-    ssh_command="$ssh_command -o \"${SSH_USER}\""
+  ssh_command="/usr/bin/ssh -v -T -N -o StrictHostKeyChecking=false -o ServerAliveInterval=${SSH_SERVER_CHECK_INTERVAL}"
+  ssh_command="$ssh_command -o User=$user"
+  if [ -f "$credentials" ]; then
+    ssh_command="$ssh_command -i \"$credentials\""
+  else
+    ssh_command="sshpass -p \"$credentials\" $ssh_command"
   fi
-  if [ -f "${SSH_KEY_FILE}" ]; then
-    ssh_command="$ssh_command -i \"${SSH_KEY_FILE}\""
-  fi
-  ssh_command="$ssh_command -L *:${LOCAL_PORT}:${REMOTE_HOST}:${REMOTE_PORT} ${REMOTE_HOST}"
-  exec $ssh_command
+  ssh_command="$ssh_command -L *:$local_port:$remote_host:$remote_port $remote_host"
+  $ssh_command &
+  pids="$pids $!"
 
-fi
+done
+
+# Wait for all spawned processes to complete
+wait $pids
