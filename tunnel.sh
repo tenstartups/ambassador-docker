e275a740654462bd85bd56cdd5d9d7bcaecbc731
Marc Lennox <marc.lennox@gmail.com>
2015-06-25 12:44:25 -0400

    Fixed the regex

--- tunnel.sh
+++ tunnel.sh
@@ -5,14 +5,14 @@ set -e
 SSH_SERVER_CHECK_INTERVAL=${SSH_SERVER_CHECK_INTERVAL:-30}
 
 # Look for insecure tunnel specifications and spawn them
-env | grep -E '^TCP_TUNNEL_PORT_([0-9]+)=' | sed -E 's/^TCP_TUNNEL_PORT_([0-9]+)=(.+):([0-9]+)/\1 \2 \3/' | while read -r local_port remote_host remote_port ; do
+env | grep -E '^TCP_TUNNEL_[A-Z]+_([0-9]+)=' | sed -E 's/^TCP_TUNNEL_[A-Z]+_([0-9]+)=(.+):([0-9]+)/\1 \2 \3/' | while read -r local_port remote_host remote_port ; do
   echo "Opening insecure tunnel *:$local_port -> $remote_host:$remote_port"
   /usr/bin/socat TCP4-LISTEN:$local_port,fork,reuseaddr TCP4:$remote_host:$remote_port &
   pids="$pids $!"
 done
 
 # Look for secure tunnel specifications and spawn them
-env | grep -E '^SSH_TUNNEL_PORT_([0-9]+)=' | sed -E 's/^SSH_TUNNEL_PORT_([0-9]+)=(.+):(.+)@(.+):([0-9]+)/\1 \2 \3 \4 \5/' | while read -r local_port user credentials remote_host remote_port ; do
+env | grep -E '^SSH_TUNNEL_[A-Z]+_([0-9]+)=' | sed -E 's/^SSH_TUNNEL_[A-Z]+_([0-9]+)=(.+):(.+)@(.+):([0-9]+)/\1 \2 \3 \4 \5/' | while read -r local_port user credentials remote_host remote_port ; do
 
   ssh_command="/usr/bin/ssh -v -T -N -o StrictHostKeyChecking=false -o ServerAliveInterval=${SSH_SERVER_CHECK_INTERVAL}"
   ssh_command="$ssh_command -o User=$user"
