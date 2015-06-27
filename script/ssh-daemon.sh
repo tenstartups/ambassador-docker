5960b807dcc14aacce56278b296353623a15e7e2
Marc Lennox <marc.lennox@gmail.com>
2015-06-26 21:18:22 -0400

    Added debugging

--- script/ssh-daemon.sh
+++ script/ssh-daemon.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 set -e
 
+# Set environment variables
+SSH_HOST_KEY_FILE=${SSH_HOST_KEY_FILE:-/etc/ssh/ssh_host_rsa_key}
+
 command="/usr/sbin/sshd -D"
-if [ "${SSH_DEBUG}" = "true" ]; then
-  command="$command -v"
+if [ "${SSH_DEBUG}" = "1" ]; then
+  command="$command -d -e"
 fi
+if [ "${SSH_DEBUG}" = "2" ]; then
+  command="$command -d -d -e"
+fi
+if [ "${SSH_DEBUG}" = "3" ]; then
+  command="$command -d -d -d -e"
+fi
+command="$command -h ${SSH_HOST_KEY_FILE}"
 
 $command
