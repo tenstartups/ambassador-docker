548fbc9b57505031462c6614d68c0701d29d3ff3
Marc Lennox <marc.lennox@gmail.com>
2015-07-07 11:06:25 -0400

    Added configurable ssh authorized key file

--- script/ssh-daemon.sh
+++ script/ssh-daemon.sh
@@ -3,10 +3,17 @@ set -e
 
 # Set environment variables
 SSH_HOST_KEY_FILE=${SSH_HOST_KEY_FILE:-/etc/ssh/ssh_host_rsa_key}
+SSH_AUTHORIZED_KEYS_FILE=${SSH_AUTHORIZED_KEYS_FILE:-/root/.ssh/authorized_keys}
 
 # Generate an SSH key if none is present
 if ! [ -f "${SSH_HOST_KEY_FILE}" ]; then
   ssh-keygen -t rsa -b 4096 -f "${SSH_HOST_KEY_FILE}" -N '' -C ''
+fi
+
+# Create a symlink to the authorized keys file if not the DEFAULT_SSH_PORT
+if [ -f "${SSH_AUTHORIZED_KEYS_FILE}" ] && [ "${SSH_AUTHORIZED_KEYS_FILE}" != "/root/.ssh/authorized_keys" ]; then
+  rm -f "/root/.ssh/authorized_keys"
+  ln -fs "${SSH_AUTHORIZED_KEYS_FILE}" "/root/.ssh/authorized_keys"
 fi
 
 command="/usr/sbin/sshd -D"
